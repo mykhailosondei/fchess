2663ffbb347ab4df69054d17fde8979a1fc1206b
mykhailosondei <mykhailosondei@gmail.com>
2025-05-20 00:47:29 -0400

    refactor strategy signature

--- fchess/CLI/Commands/ICommandStrategy.fs
+++ fchess/CLI/Commands/ICommandStrategy.fs
@@ -3,4 +3,4 @@ module fchess.CLI.Commands.ICommandStrategy
 open fchess.Core.Game
 
 type ICommandStrategy =
-    abstract member Execute: state: Game * args: string -> Result<Game, string> 
+    abstract member Execute: state: Game -> args: string -> Result<Game, string>
